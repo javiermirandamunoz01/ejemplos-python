34f0bfd6467464deb318205bb2f44e325b73a08b
javiermirandamunoz01 <javiermirandamunoz01@gmail.com>
2026-03-05 01:09:34 +0000

    probando

---
+++ main.py
@@ -0,0 +1,34 @@
+
+
+print("hola mundo")
+
+# variables:
+
+nombre = "javier"
+edad = 33
+altura = 1.75
+es_profesor = False
+ciudad = "rancagua"
+genero = ["masculino","femenino","otro"]
+
+
+#TIPOS DE DATOS
+
+str #esto es un texto
+int #esto es un entero
+float #esto es un flotante
+bool #esto es verdadero o falso
+
+#input
+#input()
+
+
+texto =input()
+print(texto)
+
+
+
+
+
+
+
